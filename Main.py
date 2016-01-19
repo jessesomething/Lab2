775552aeb5edfe872101be6d2b503f22b5da721f
jessesomething <jessespalding@gmail.com>
2016-01-19 16:28:40 -0600

    Initial commit Baseline code

--- Main.py
+++ Main.py
@@ -2,43 +2,67 @@ import sqlite3
 
 option = ''
 
-def menu(option):
+conn = sqlite3.connect('bobbleheads.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS Collectors (name TEXT, bobbleheads int, country TEXT)''')
+# c.execute("INSERT INTO collectors VALUES ('Jesse', '10')")
+conn.commit()
+
+def menu():
     print("Welcome to the Bobblehead Collector!\n\nMenu\n1. Add new collector\n2. Add bobbleheads\n"
           "3. Search records\n4. Delete records\n5. View records")
-    option = input("Select an option: ")
-    optionInt = int(option)
+    optionInput = input("Select an option: ")
+    optionInt = int(optionInput)
     if optionInt <= 1 and optionInt >=4:
-        menu(option)
-    else: return option
+        menu()
+    else: return optionInt
 
-def connectCreate():
-    conn = sqlite3.connect('bobbleheads.db')
-    c = conn.cursor()
-
-    c.execute('''CREATE TABLE IF NOT EXISTS Collectors (name TEXT, bobbleheads INTEGER, country TEXT)''')
-    conn.commit()
+# def connectCreate():
+#     conn = sqlite3.connect('bobbleheads.db')
+#     c = conn.cursor()
+#
+#     c.execute('''CREATE TABLE IF NOT EXISTS Collectors (name TEXT, bobbleheads int, country TEXT)''')
+#     conn.commit()
 
 def recordInput():
     collector = input("Collector name: ")
     bobbles = input("Bobbleheads: ")
+    bobblesInt = int(bobbles)
     country = input("Country: ")
 
-    recordData = [(collector, bobbles, country)]
+    recordData = [(collector, bobblesInt, country)]
 
     return recordData
+
+def bobbleInput():
+    bobbles = input("Bobbles to add: ")
+    bobblesInt = int(bobbles)
+    return bobblesInt
+
+def nameInput():
+    name = input("Name to search/add to: ")
+    return name
+
+def countryInput():
+    country = input("Country: ")
+    return country
 
 def newCollector():
     conn = sqlite3.connect('bobbleheads.db')
     c = conn.cursor()
-    c.executemany('INSERT INTO Collectors VALUES (?,?,?)', recordInput())
+    recordData = recordInput()
+
+    c.executemany('INSERT INTO Collectors VALUES (?,?,?)', recordData)
+    conn.commit()
 
 def addBobble():
     conn = sqlite3.connect('bobbleheads.db')
     c = conn.cursor()
-    data = recordInput()
-    nameData = data[0]
-    bobbleData = data[1]
-    c.execute('INSERT INTO Collectors(bobbleheads) VALUES (?) WHERE name = ' + nameData, bobbleData)
+    name = nameInput()
+    c.execute('UPDATE Collectors'
+              'SET bobbleheads = ' + bobbleInput() +
+              'WHERE name LIKE '' + name + ''')
+    conn.commit()
 
 def search():
     return 0
@@ -47,10 +71,14 @@ def delete():
     return 0
 
 def viewRecords():
+    conn = sqlite3.connect('bobbleheads.db')
+    c = conn.cursor()
     c.execute("SELECT * FROM Collectors")
     print(c.fetchall())
 
-option = menu(option)
+# viewRecords()
+
+option = menu()
 
 if option == 1:
     newCollector()
@@ -62,13 +90,5 @@ if option == 4:
     delete()
 if option == 5:
     viewRecords()
-
-connectCreate()
-
-conn = sqlite3.connect('bobbleheads.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS Collectors (name TEXT, bobbleheads INTEGER)''')
-# c.execute("INSERT INTO collectors VALUES ('Jesse', '10')")
-conn.commit()
 
 conn.close()
